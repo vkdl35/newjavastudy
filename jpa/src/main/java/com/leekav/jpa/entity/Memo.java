1b4225d80b31e92cc0b8785058956212b7266a57
tjoeun <tjoeun@DESKTOP-CKPDHP5>
2022-10-18 09:28:35 +0900

    jpa

---
+++ jpa/src/main/java/com/leekav/jpa/entity/Memo.java
@@ -0,0 +1,33 @@
+package com.leekav.jpa.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="tbl_memo")
+
+@ToString
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Memo {
+	//기본키 서렁 - Hibernate 가 알아서 결정
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long mno;
+	@Column(length=200, nullable=false)
+	private String memoText;
+}
